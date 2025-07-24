3b4f3630bde7d4ae533cd90ebf0ad4c9f4eb393c
pkamenga44 <pkamenga1985@gmail.com>
2025-07-24 16:21:31 -0500

    updating top to 80

--- CS416-Narrative-Project/script_Nitendo_highprice.js
+++ CS416-Narrative-Project/script_Nitendo_highprice.js
@@ -1,5 +1,5 @@
 // Set dimensions and margins for the chart
-const margin = { top: 70, right: 60, bottom: 40, left: 80 };
+const margin = { top: 80, right: 60, bottom: 40, left: 80 };
 const width = 1200 - margin.left - margin.right;
 const height = 500 - margin.top - margin.bottom;
 
