49ec356505d33559f76dc2696bbe19dfec168a0e
pkamenga44 <pkamenga1985@gmail.com>
2025-07-24 15:56:39 -0500

    revert back width to 1200

--- script_Nitendo_highprice.js
+++ script_Nitendo_highprice.js
@@ -1,6 +1,6 @@
 // Set dimensions and margins for the chart
 const margin = { top: 70, right: 60, bottom: 40, left: 80 };
-const width = 800 - margin.left - margin.right;
+const width = 1200 - margin.left - margin.right;
 const height = 500 - margin.top - margin.bottom;
 
 // Set up the x and y scales
